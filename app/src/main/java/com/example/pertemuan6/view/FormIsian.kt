9e2a6355ca0614ce503184b2eb8e5199370c9024
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-06 14:08:27 +0700

    membuat fun composable di form isian

--- app/src/main/java/com/example/pertemuan6/view/FormIsian.kt
+++ app/src/main/java/com/example/pertemuan6/view/FormIsian.kt
@@ -1,2 +1,9 @@
 package com.example.pertemuan6.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick: () -> Unit
+) {
