b66e0e7c5e8d17ff99d677e6c71874a8101ed439
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-06 14:09:04 +0700

    membuat fun composable, scaffold di form isian

--- app/src/main/java/com/example/pertemuan6/view/FormIsian.kt
+++ app/src/main/java/com/example/pertemuan6/view/FormIsian.kt
@@ -7,3 +7,15 @@ fun FormIsian(
     jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
     OnSubmitBtnClick: () -> Unit
 ) {
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home), color = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    )
+}
