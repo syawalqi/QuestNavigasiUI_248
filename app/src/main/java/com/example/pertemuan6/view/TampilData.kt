c145b83c9999f2ccd66b74e5cea56e331a1fcdae
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-06 14:18:27 +0700

    fix minor changes

--- app/src/main/java/com/example/pertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/pertemuan6/view/TampilData.kt
@@ -1,5 +1,18 @@
 @file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.pertemuan6.view
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pertemuan6.R
 
 
 @Composable
