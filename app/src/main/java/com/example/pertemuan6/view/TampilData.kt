7b212e017adea3ca080873078e52b0dbc29d8c62
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-06 14:17:59 +0700

    membuat spacer dan button

--- app/src/main/java/com/example/pertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/pertemuan6/view/TampilData.kt
@@ -46,3 +46,14 @@ fun TampilData(
                     }
                 }
             }
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onBackBtnClick
+            ) {
+                Text(text = stringResource(id = R.string.back))
+            }
+        }
+    }
+}
