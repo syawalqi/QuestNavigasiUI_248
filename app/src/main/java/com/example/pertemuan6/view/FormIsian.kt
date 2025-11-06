bb79e3c95a59304ef62ca8bc9d074da2ca51cf87
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-06 14:13:18 +0700

    fix minor changes di form isian

--- app/src/main/java/com/example/pertemuan6/view/FormIsian.kt
+++ app/src/main/java/com/example/pertemuan6/view/FormIsian.kt
@@ -1,6 +1,15 @@
 package com.example.pertemuan6.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.pertemuan6.R
 
 @Composable
 fun FormIsian(
