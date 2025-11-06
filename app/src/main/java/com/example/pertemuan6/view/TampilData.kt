ac5169e21aa6fd1c949ce51f25e18ddd1dc527da
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-06 14:15:48 +0700

    membuat list di TampilData.kt

--- app/src/main/java/com/example/pertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/pertemuan6/view/TampilData.kt
@@ -5,4 +5,9 @@ package com.example.pertemuan6.view
 @Composable
 fun TampilData(
     onBackBtnClick: () -> Unit
-)
+){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
+    )
